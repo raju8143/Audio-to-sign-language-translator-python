ea3945e8ecef9200ec02778d8d2b20b6ffce72d2
Raju <rajunetha2004@gmail.com>
2026-07-12 12:18:46 +0530

    Configure PostgreSQL for Vercel deployment

--- A2SL/settings.py
+++ A2SL/settings.py
@@ -1,3 +1,5 @@
+import os
+import dj_database_url
 """
 Django settings for A2SL project.
 
@@ -80,13 +82,24 @@ WSGI_APPLICATION = 'A2SL.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600,
+            conn_health_checks=True,
+            ssl_require=True,
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
---
+++ build_files.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e
+
+python manage.py collectstatic --noinput
+python manage.py migrate
